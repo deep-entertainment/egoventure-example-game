cc2a32c80eb1ccb8462a691af7b4f14f0175a798
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-02-01 01:52:55 +0100

    Mock-up dialog scene.

--- game_state.gd
+++ game_state.gd
@@ -2,3 +2,6 @@ class_name GameState
 extends BaseState
 
 export(bool) var has_keys = false
+export(bool) var DialogHotspot2 = false
+export(bool) var DialogHotspot3 = false
+export (int) var bro1_q2
