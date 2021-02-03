41e816316dbc0ea8df100bf4cbd3c3b26027e73b
Dennis Ploeger <develop@dieploegers.de>
2021-02-03 19:09:10 +0100

    feat: Min waiting time for location changes, hidden cursor, default hotspot type

--- addons/mdna_core/nodes/map_hotspot.gd
+++ addons/mdna_core/nodes/map_hotspot.gd
@@ -5,6 +5,10 @@ class_name MapHotspot, "res://addons/mdna_core/images/map_hotspot.svg"
 extends Hotspot
 
 
+# The minimum time to wait when switching scenes
+const MIN_WAITING_TIME = 4
+
+
 # The loading image to show while the scenes for the new location are
 # cached
 export(Texture) var loading_image
@@ -18,6 +22,7 @@ export(String) var location = ""
 
 # Connect the pressed signal
 func _init():
+	hotspot_type = Cursors.Type.MAP
 	connect("pressed", self, "_on_pressed")
 
 
@@ -25,12 +30,24 @@ func _init():
 func _on_pressed():
 	release_focus()
 	if MdnaInventory.selected_item == null:
+		Speedy.hidden = true
 		accept_event()
 		Boombox.play_music(location_music)
 		MdnaCore.target_view = target_view
 		MdnaCore.current_location = location
 		WaitingScreen.set_image(loading_image)
+		var start = OS.get_ticks_msec()
 		var caches = MdnaCore.update_cache(target_scene, true)
 		if caches > 0:
 			yield(MdnaCore, "queue_complete")
+		var end = OS.get_ticks_msec()
+		if ((end - start) / 1000) < MIN_WAITING_TIME:
+			WaitingScreen.show()
+			yield(
+				get_tree().create_timer(
+					ceil(MIN_WAITING_TIME-((end - start) / 1000))
+				),
+				"timeout"
+			)
+		Speedy.hidden = false
 		MdnaCore.change_scene(target_scene)
