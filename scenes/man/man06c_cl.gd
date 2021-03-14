965f2ed22a99732e595250c61bc793ce0f5db4e0
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-03-14 20:12:56 +0100

    Hotspots totally updated.

--- scenes/man/man06c_cl.gd
+++ scenes/man/man06c_cl.gd
@@ -30,6 +30,10 @@ func _on_TriggerHotspot_item_used(item):
 	EgoVenture.change_scene("res://scenes/man/man06c_op.tscn")
 
 
+
+
+
 func _on_man_hand_on_insex_pressed():
 	Boombox.play_effect(preload("res://sounds/man/man_bedroom_drawer_op.ogg"))
 	EgoVenture.change_scene("res://scenes/man/man06c_op.tscn")
+
--- scenes/man/man11.gd
+++ scenes/man/man11.gd
@@ -26,26 +26,6 @@ func _ready():
 	Cursors.override(Cursors.Type.CUSTOM1, preload("res://images/mouse/right_corner_down.png"), Vector2(32, 32))
 
 
-func _on_Hotspot_pressed():
-	Speedy.hidden = true
-	Boombox.ignore_pause = true
-	get_tree().paused = true
-	$man_stairs.show()
-	Boombox.play_effect(preload("res://sounds/man/man_stairs.ogg"))
-	yield(get_tree().create_timer(1.5), "timeout")
-	get_tree().paused = false
-	Boombox.ignore_pause = false
-	EgoVenture.target_view = "back"
-	Speedy.hidden = false
-	EgoVenture.change_scene("res://scenes/man/man05.tscn")
-
-
-func _on_Hotspot3_pressed():
-	Boombox.play_effect(preload("res://sounds/man/man_upper_boiler_room_op.ogg"))
-	EgoVenture.target_view = "left"
-	EgoVenture.change_scene("res://scenes/man/man11d_op.tscn")
-
-
 func _on_Hotspot4_pressed():
 	Boombox.ignore_pause = true
 	get_tree().paused = true
@@ -74,3 +54,33 @@ func _on_TriggerHotspot_item_used(item):
 func _exit_tree():
 	Cursors.reset(Cursors.Type.CORNER_LEFT)
 
+
+
+func _on_Hotspot2_pressed():
+	Boombox.ignore_pause = true
+	get_tree().paused = true
+	Boombox.play_effect(preload("res://sounds/man/man_upper_hall_boxroom_op.ogg"))
+	yield(get_tree().create_timer(0.4), "timeout")
+	get_tree().paused = false
+	Boombox.ignore_pause = false
+	EgoVenture.change_scene("res://scenes/man/man11b_op.tscn")
+
+
+func _on_Hotspot3_pressed():
+	Boombox.play_effect(preload("res://sounds/man/man_upper_boiler_room_op.ogg"))
+	EgoVenture.target_view = "left"
+	EgoVenture.change_scene("res://scenes/man/man11d_op.tscn")
+
+
+func _on_Hotspot_pressed():
+	Speedy.hidden = true
+	Boombox.ignore_pause = true
+	get_tree().paused = true
+	$man_stairs.show()
+	Boombox.play_effect(preload("res://sounds/man/man_stairs.ogg"))
+	yield(get_tree().create_timer(1.5), "timeout")
+	get_tree().paused = false
+	Boombox.ignore_pause = false
+	EgoVenture.target_view = "back"
+	Speedy.hidden = false
+	EgoVenture.change_scene("res://scenes/man/man05.tscn")
