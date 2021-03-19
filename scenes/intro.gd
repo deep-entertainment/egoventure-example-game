2fc3234995ea25da171b6c2dfdb687f0dfe9bd63
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-03-19 22:29:18 +0100

    Minor changes.

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -2,12 +2,13 @@ extends Control
 
 func _ready():
 	Speedy.hidden = true
+	EgoVenture.game_started = true
 	Inventory.disable()
 	MainMenu.saveable = true
 	$cred2.hide()
 	Boombox.ignore_pause = true
 	get_tree().paused = true
-	Boombox.play_effect(preload("res://music/tunnel.ogg"))
+	Boombox.play_music(preload("res://music/tunnel.ogg"))
 	yield(get_tree().create_timer(7), "timeout")
 	$cred2.show()
 	yield(get_tree().create_timer(7), "timeout")
