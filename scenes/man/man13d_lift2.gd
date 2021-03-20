00ad936eab71712c4a2cf623b81c3a945800cc28
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-03-20 02:20:20 +0100

    Continue state set.

--- scenes/man/man13d_lift2.gd
+++ scenes/man/man13d_lift2.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func _ready():
 	EgoVenture.check_cursor()
+	EgoVenture.reset_continue_state()
 	Cursors.override(Cursors.Type.MAP, preload("res://images/mouse/common.png"), Vector2(32, 32))
 	Boombox.play_music(preload("res://music/blue.ogg"))
 	Notepad.finished_step(6, 1)
