09e7f1c195576f059d9cc55d1fa18881e7b83285
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-03-22 13:09:50 +0100

    Intro scenes (logo and opening credits) made skippable frame-by-frame.

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -8,12 +8,19 @@ func _ready():
 	MainMenu.saveable = true
 	$cred2.hide()
 	Boombox.ignore_pause = true
-	get_tree().paused = true
 	Boombox.play_music(preload("res://music/tunnel.ogg"))
 	yield(get_tree().create_timer(7), "timeout")
 	$cred2.show()
 	yield(get_tree().create_timer(7), "timeout")
-	get_tree().paused = false
 	Boombox.ignore_pause = false
+	Speedy.hidden = false
+	EgoVenture.change_scene("res://scenes/misc/map_info.tscn")
+
+
+func _on_cred1_pressed():
+	$cred2.show()
+
+
+func _on_cred2_pressed():
 	Speedy.hidden = false
 	EgoVenture.change_scene("res://scenes/misc/map_info.tscn")
--- scenes/logo.gd
+++ scenes/logo.gd
@@ -5,9 +5,13 @@ func _ready():
 	Speedy.hidden = true
 	Inventory.disable()
 	MainMenu.disabled = true
-	get_tree().paused = true
 	yield(get_tree().create_timer(5), "timeout")
-	get_tree().paused = false
+	MainMenu.disabled = false
+	Speedy.hidden = false
+	EgoVenture.change_scene("res://scenes/menu.tscn")
+
+
+func _on_Hotspot_pressed():
 	MainMenu.disabled = false
 	Speedy.hidden = false
 	EgoVenture.change_scene("res://scenes/menu.tscn")
