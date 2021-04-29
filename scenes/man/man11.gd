15d5a6aebab87ec59ae5b979d05724277d08a25f
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-04-30 00:51:56 +0200

    Another custom cursor corrected in game.

--- scenes/man/man11.gd
+++ scenes/man/man11.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 func _process(_delta):
-	Cursors.override(Cursors.Type.CORNER_LEFT, preload("res://images/mouse/common.png"), Vector2(32, 32))
 	var state = EgoVenture.state
 	if state.upper_door_hs:
 		$upper_door_hs.hide()
@@ -49,9 +48,6 @@ func _on_TriggerHotspot_item_used(item):
 	Notepad.finished_step(2, 2)
 	EgoVenture.change_scene("res://scenes/man/man12.tscn")
 	
-	
-func _exit_tree():
-	Cursors.reset(Cursors.Type.CORNER_LEFT)
 
 
 func _on_Hotspot2_activate():
