1176501eeefd5790bc58bb43567e319adc0fd9b2
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-05-11 12:55:09 +0200

    Reverted example scene for hotspot anomaly.

--- scenes/man/man21.gd
+++ scenes/man/man21.gd
@@ -1,18 +1,5 @@
 extends Node2D
 
 func _ready():
-	check_hotspots()
+	pass
 
-func _process(_delta):
-	check_hotspots()
-
-func check_hotspots():
-	var state = EgoVenture.state
-	if state.temp_hand:
-		$temp_hand.hide()
-	else:
-		$temp_hand.show()
-
-
-func _on_temp_hand_activate():
-	(EgoVenture.state as GameState).temp_hand = true
