881d3676f6f462f859b3e3f3de5e20e33744ef0c
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-06-21 18:41:16 +0200

    Example for music bug.

--- core.gd
+++ core.gd
@@ -34,6 +34,7 @@ func _on_new_game():
 func _initialization():
 	var state = GameState.new()
 	EgoVenture.state = state
+	(EgoVenture.state as GameState).map_kevo = true
 	
 func _on_load():
 	pass
---
+++ scenes/man/man18.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+func _ready():
+	Boombox.play_music(preload("res://music/piano.ogg"))
+
