07ab43c5254656515a3597787fc30a41fbdeb890
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-07-01 08:45:38 +0200

    Example of twisted background channels.

--- scenes/man/man05.gd
+++ scenes/man/man05.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _ready():
 	$man_stairs.hide()
 	Boombox.play_music(preload("res://music/piano.ogg"))
+	Boombox.play_background(preload("res://sounds/backgrounds/sam_harris.ogg")) 
 	Cursors.override(Cursors.Type.CUSTOM1, preload("res://images/mouse/hand.png"), Vector2(46, 32))
 
 
