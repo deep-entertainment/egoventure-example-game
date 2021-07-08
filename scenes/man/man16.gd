a0ec0561e9e422f007ebcf4aa0ab23bf2925adfe
Dennis Ploeger <develop@dieploegers.de>
2021-07-08 12:41:06 +0200

    fix: Fixed view switch timing

--- scenes/man/man16.gd
+++ scenes/man/man16.gd
@@ -6,7 +6,7 @@ func _ready():
 func _on_Hotspot_activate():
 	Boombox.ignore_pause = true
 	get_tree().paused = true
-	yield(get_tree().create_timer(0.4), "timeout")
+	yield(get_tree().create_timer(1), "timeout")
 	get_tree().paused = false
 	Boombox.ignore_pause = false
 	EgoVenture.target_view = "front"
