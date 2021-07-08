7271d60b3d2c68d221d98cc6fce8636883313848
MikaelNyqvist <mikael.nyqvist@telia.com>
2021-07-08 11:18:57 +0200

    Example for strange blackout frames.

--- scenes/man/man16.gd
+++ scenes/man/man16.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 func _ready():
-	Parrot.connect("finished_dialog", self, "_on_finished_dialog")
-	_update_hotspots()
-
-func _update_hotspots():
 	pass
 
-func _on_finished_dialog(dialog_id: String):
-	
-	_update_hotspots()
+func _on_Hotspot_activate():
+	Boombox.ignore_pause = true
+	get_tree().paused = true
+	yield(get_tree().create_timer(0.4), "timeout")
+	get_tree().paused = false
+	Boombox.ignore_pause = false
+	EgoVenture.target_view = "front"
+	EgoVenture.change_scene("res://scenes/man/man17.tscn")
